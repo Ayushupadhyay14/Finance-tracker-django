a3ae1d7a686d36709481ca85198ca044d5c943a7
Ayushupadhyay14 <Ayushupadhyay88151528@gmail.com>
2025-05-28 16:15:13 +0530

    run ok 200

--- djfintracker/settings.py
+++ djfintracker/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-fxdwg@6x!ine%1qmr14y65rzoy6w7ko=n=^wq+tcgsyz(h9+#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -74,12 +73,12 @@ WSGI_APPLICATION = 'djfintracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
